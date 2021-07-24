0dfb9cb4ae63a6b06d1c01dff00299dc5b311cd2
Adrian <adriantutuianu01@gmail.com>
2021-07-24 22:50:17 +0100

    Created the initial list

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("works");
+const list = [
+  { name: "Adrian", surname: "Tut", phone: "0766566398" },
+  { name: "John", surname: "Doe", phone: "07645557766" },
+  { name: "Mickey", surname: "Smith", phone: "0763554358" },
+];
