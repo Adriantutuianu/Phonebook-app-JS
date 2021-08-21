94a7c87c9080e786e076b7fff5ce61b4044c748f
Adrian <adriantutuianu01@gmail.com>
2021-08-21 21:28:02 +0100

    Selected phoneList id

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const list = [
   { name: "John", surname: "Doe", phone: "07645557766" },
   { name: "Mickey", surname: "Smith", phone: "0763554358" },
 ];
+
+const phoneList = document.querySelector("#phone-list");
+console.log(phoneList);
