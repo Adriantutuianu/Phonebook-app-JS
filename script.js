c0f0a3d0ca6b021e410159e0e806643d50b09c21
Adrian <adriantutuianu01@gmail.com>
2021-08-21 22:41:09 +0100

    Added an element to array

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ const displayListElement = (item) => {
   node.appendChild(textnode); //then append the Text node to the LI node.
   phoneList.appendChild(node); //Finally append the LI node to the list.
 };
+let adi = { name: "Maria", surname: "Tut", phone: " 07927753503" };
+
+list.push(adi);
 
 // Loop through array list
 list.map((item) => {
