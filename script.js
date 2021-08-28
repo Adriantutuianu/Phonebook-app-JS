e16c5a450ff5093f5259aced81356e3d274ca67a
Adrian <adriantutuianu01@gmail.com>
2021-08-28 22:09:14 +0100

    Refactor displaying items

--- script.js
+++ script.js
@@ -5,66 +5,69 @@ let list = [
   { name: "George", surname: "Smith", phone: "0763554358" },
 ];
 
-const phoneList = document.querySelector("#phone-list");
+function app(contactList) {
+  const phoneList = document.querySelector("#phone-list");
 
-const displayListElement = (item) => {
-  let node = document.createElement("LI"); //First create an LI node,
-  let textnode = document.createTextNode(
-    `${item.name} ${item.surname} - ${item.phone}`
-  ); //then create a Text node,
-  node.appendChild(textnode); //then append the Text node to the LI node.
-  phoneList.appendChild(node); //Finally append the LI node to the list.
-};
-
-const clearDisplayedList = () => {
-  // delete all displayed elements
-  while (phoneList.firstChild) {
-    phoneList.removeChild(phoneList.firstChild);
-  }
-};
-
-const newContactForm = document.querySelector(".new-contact-form");
-
-const addNewContact = (e) => {
-  e.preventDefault();
-  let firstName = document.querySelector("#fname");
-  let lastName = document.querySelector("#lname");
-  let phoneNumber = document.querySelector("#phone");
-
-  const newContact = {
-    name: firstName.value,
-    surname: lastName.value,
-    phone: phoneNumber.value,
+  const displayListElement = (item) => {
+    let node = document.createElement("LI"); //First create an LI node,
+    let textnode = document.createTextNode(
+      `${item.name} ${item.surname} - ${item.phone}`
+    ); //then create a Text node,
+    node.appendChild(textnode); //then append the Text node to the LI node.
+    phoneList.appendChild(node); //Finally append the LI node to the list.
   };
 
-  clearDisplayedList();
+  const clearDisplayedList = () => {
+    // delete all displayed elements
+    while (phoneList.firstChild) {
+      phoneList.removeChild(phoneList.firstChild);
+    }
+  };
 
-  // generate a new list
-  const newList = [...list, newContact];
+  const newContactForm = document.querySelector(".new-contact-form");
 
-  // re display new list
-  newList.map((item) => {
+  const addNewContact = (e) => {
+    e.preventDefault();
+    let firstName = document.querySelector("#fname");
+    let lastName = document.querySelector("#lname");
+    let phoneNumber = document.querySelector("#phone");
+
+    const newContact = {
+      name: firstName.value,
+      surname: lastName.value,
+      phone: phoneNumber.value,
+    };
+
+    clearDisplayedList();
+
+    // generate a new list
+    const newList = [...list, newContact];
+
+    // hide the form
+    newContactForm.classList.remove("is-visible");
+
+    // clear input values
+    firstName.value = "";
+    lastName.value = "";
+    phoneNumber.value = "";
+
+    app(newList); // rerender app
+  };
+
+  newContactForm.addEventListener("submit", addNewContact);
+
+  // Loop through array list
+  contactList.map((item) => {
     displayListElement(item);
   });
-  // hide the form
-  newContactForm.classList.remove("is-visible");
 
-  firstName.value = "";
-  lastName.value = "";
-  phoneNumber.value = "";
-};
+  const newContactBtn = document.querySelector(".btn-new-contact");
 
-newContactForm.addEventListener("submit", addNewContact);
+  const enableNewContactForm = () => {
+    newContactForm.classList.add("is-visible");
+  };
 
-// Loop through array list
-list.map((item) => {
-  displayListElement(item);
-});
+  newContactBtn.addEventListener("click", enableNewContactForm);
+}
 
-const newContactBtn = document.querySelector(".btn-new-contact");
-
-const enableNewContactForm = () => {
-  newContactForm.classList.add("is-visible");
-};
-
-newContactBtn.addEventListener("click", enableNewContactForm);
+app(list);
